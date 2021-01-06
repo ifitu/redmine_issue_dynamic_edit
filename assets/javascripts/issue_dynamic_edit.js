65ee4bd2286623b6e0b7190a738b670a70435b2b
Wenfeng CAI <wenfeng.cai@ifi-corp.com>
2021-01-06 10:06:03 +0800

    Use another CDN for Fontawesome

--- assets/javascripts/issue_dynamic_edit.js
+++ assets/javascripts/issue_dynamic_edit.js
@@ -36,7 +36,7 @@ if (!document.getElementById(cssId)) {
 	link.id    = cssId;
 	link.rel   = 'stylesheet';
 	link.type  = 'text/css';
-	link.href  = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
+	link.href  = 'https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css';
 	link.media = 'all';
 	head.appendChild(link);
 }
